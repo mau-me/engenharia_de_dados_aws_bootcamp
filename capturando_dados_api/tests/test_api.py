1d32b20110c2f5a97e205005c22f89d114eef76f
mau-me <mauriciosm95@gmail.com>
2024-03-18 15:12:43 -0300

    tests: cria testes para a Trades API

--- capturando_dados_api/tests/test_api.py
+++ capturando_dados_api/tests/test_api.py
@@ -18,10 +18,22 @@ class TestTickersApi:
 class TestTradesApi:
     @pytest.mark.parametrize(
         'coin, date_from, date_to, expected',
-        [('BTC-BRL', None, None, 'https://api.mercadobitcoin.net/api/v4/BTC-BRL/trades')]
+        [
+            ('BTC-BRL', None, None,
+             'https://api.mercadobitcoin.net/api/v4/BTC-BRL/trades'),
+            ('BTC-BRL', datetime.datetime(2023, 3, 17), None,
+                'https://api.mercadobitcoin.net/api/v4/BTC-BRL/trades/?from=1679022000'),
+            ('BTC-BRL', datetime.datetime(2023, 3, 16), datetime.datetime(2023, 3, 17),
+                'https://api.mercadobitcoin.net/api/v4/BTC-BRL/trades/?from=1678935600,to=1679022000')
+        ]
     )
     def test_get_endpoint_trades(self, coin, date_from, date_to, expected):
-        actual = TradesApi(
-            'BTC-BRL')._get_endpoint(date_from=date_from, date_to=date_to)
+        actual = TradesApi(coin)._get_endpoint(
+            date_from=date_from, date_to=date_to)
 
         assert actual == expected
+
+    def test_get_endpoint_trades_datefrom_greater_dateto(self):
+        with pytest.raises(RuntimeError):
+            TradesApi("TEST")._get_endpoint(
+                date_from=datetime.datetime(2023, 3, 17), date_to=datetime.datetime(2023, 3, 16))
