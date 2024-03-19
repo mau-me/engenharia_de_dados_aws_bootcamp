bcea352fd5a129b2828beff10f98c0ac287bf297
mau-me <mauriciosm95@gmail.com>
2024-03-19 11:44:57 -0300

    tests: add fixture nos tests

--- capturando_dados_api/tests/test_ingestors.py
+++ capturando_dados_api/tests/test_ingestors.py
@@ -5,35 +5,38 @@ import pytest
 from ingestao import DataIngestor, DataWriter
 
 
+@pytest.fixture
+@patch('ingestao.DataIngestor.__abstractmethods__', set())
+def data_ingestor_fixture():
+    return DataIngestor(writer=DataWriter, coins=[
+        'TESTE01', 'TESTE02'], default_start_date=datetime.datetime(2024, 3, 17))
+
+
 @patch('ingestao.DataIngestor.__abstractmethods__', set())
 class TestIngerstors:
 
-    def test_checkpoint_filename(self):
-        actual = DataIngestor(writer=DataWriter, coins=[
-                              'TESTE01', 'TESTE02'], default_start_date=datetime.date(2024, 3, 17))._checkpoint_filename
+    def test_checkpoint_filename(self, data_ingestor_fixture):
+        actual = data_ingestor_fixture._checkpoint_filename
 
         expected = 'DataIngestor.checkpoint'
         assert actual == expected
 
-    def test_load_checkpoint_existing(self):
-        actual = DataIngestor(writer=DataWriter, coins=[
-                              'TESTE01', 'TESTE02'], default_start_date=datetime.datetime(2024, 3, 17))._load_checkpoint()
+    def test_load_checkpoint_existing(self, data_ingestor_fixture):
+        actual = data_ingestor_fixture._load_checkpoint()
 
         expected = datetime.datetime(2024, 3, 17)
         assert actual == expected
 
     @patch('builtins.open', new_callable=mock_open, read_data='2021-06-25')
-    def test_load_checkpoint_nonexistent(self, mock):
-        actual = DataIngestor(writer=DataWriter, coins=[
-                              'TESTE01', 'TESTE02'], default_start_date=datetime.datetime(2024, 3, 17))._load_checkpoint()
+    def test_load_checkpoint_nonexistent(self, mock, data_ingestor_fixture):
+        actual = data_ingestor_fixture._load_checkpoint()
 
         expected = datetime.datetime(2024, 3, 17)
         assert actual == expected
 
     @patch('ingestao.DataIngestor._write_checkpoint', return_value=None)
-    def test_update_checkpoint_update(self, mock):
-        data_ingestor = DataIngestor(writer=DataWriter, coins=[
-            'TESTE01', 'TESTE02'], default_start_date=datetime.datetime(2024, 3, 16))
+    def test_update_checkpoint_update(self, mock, data_ingestor_fixture):
+        data_ingestor = data_ingestor_fixture
         data_ingestor._update_checkpoint(value=datetime.datetime(2024, 3, 17))
 
         actual = data_ingestor._checkpoint
@@ -42,9 +45,8 @@ class TestIngerstors:
         assert actual == expected
 
     @patch('ingestao.DataIngestor._write_checkpoint', return_value=None)
-    def test_update_checkpoint_written(self, mock):
-        data_ingestor = DataIngestor(writer=DataWriter, coins=[
-            'TESTE01', 'TESTE02'], default_start_date=datetime.datetime(2024, 3, 16))
+    def test_update_checkpoint_written(self, mock, data_ingestor_fixture):
+        data_ingestor = data_ingestor_fixture
         data_ingestor._update_checkpoint(value=datetime.datetime(2024, 3, 17))
 
         mock.assert_called_once()
